4b49603b9c8a3e8466a79ef28f573d27bd43e3c2
sergei <sdmserg2021@gmail.com>
2025-11-30 13:08:11 +0300

    5. Прерывание нити.

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupted();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load : " + process[i++]);
+            if (i == process.length) {
+                i = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Загрузка завершена.");
+    }
+}
