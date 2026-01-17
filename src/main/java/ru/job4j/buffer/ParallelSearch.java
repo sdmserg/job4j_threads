a2ec8221dbc1f2285ad2f87cfb25daa651a62b90
sergei <sdmserg2021@gmail.com>
2026-01-18 02:04:57 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.buffer;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                             System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        final Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i != 3; i++) {
+                        try {
+                            queue.offer(i);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+    }
+}
--- src/main/java/ru/job4j/buffer/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/buffer/SimpleBlockingQueue.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.buffer;
 
 import java.util.Queue;
 import java.util.LinkedList;
